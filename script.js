c950651a491e8530d44c05ceb51a1a950e40d8c4
fariajesiel <fariajesiel@gmail.com>
2020-08-31 22:46:25 -0300

    Alteração no codigo do arquivo json

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function loadProdutos() {
     var produtos = [];
 
     $.getJSON('produtos.json', function (data) {
-        produtos = data;
+        produtos = data.produtos;
 
         for (i = 0; i < produtos.length; i++) {
            saida += '<div class="col-sm-2">';
@@ -27,16 +27,6 @@ function loadProdutos() {
            saida += 'Descrição';
            saida += '</div>';
            saida += '</div>';
-
-
-
-
-
-            // saida += '<div class="row">';
-            // saida += '<div class="col-lg-4 band-img">';
-            // saida += '<img src="' + produtos[i].name + '" alt="' + produtos[i].brand + '" title="' + produtos[i].imageUrl + '">';
-            // saida += '</div>';
-            // saida += '</div>';
         }
         document.getElementById('itens').innerHTML = saida;
     });
