31388145c3280c10f9a1f06d0aae4a0212a6aa80
fariajesiel <fariajesiel@gmail.com>
2020-08-31 22:19:38 -0300

    Acrescentando json para listar produtos do json

--- script.js
+++ script.js
@@ -1,4 +1,44 @@
-function rangeSlide(value){
-    
+function rangeSlide(value) {
     document.getElementById('rangeValue').innerHTML = value;
 }
+
+
+
+function loadProdutos() {
+
+    var saida = '';
+    var produtos = [];
+
+    $.getJSON('produtos.json', function (data) {
+        produtos = data;
+
+        for (i = 0; i < produtos.length; i++) {
+           saida += '<div class="col-sm-2">';
+           saida += '<img src="' + produtos[i].imageUrl;
+           saida += '</div>';
+           saida += '<div class="col-sm-7">';
+           saida += '<div class="row">';
+           saida += 'Nome do produto';
+           saida += '</div>';
+           saida += '<div class="row">';
+           saida += 'Marca (brand)';
+           saida += '</div>';
+           saida += '<div class="row">';
+           saida += 'Descrição';
+           saida += '</div>';
+           saida += '</div>';
+
+
+
+
+
+            // saida += '<div class="row">';
+            // saida += '<div class="col-lg-4 band-img">';
+            // saida += '<img src="' + produtos[i].name + '" alt="' + produtos[i].brand + '" title="' + produtos[i].imageUrl + '">';
+            // saida += '</div>';
+            // saida += '</div>';
+        }
+        document.getElementById('itens').innerHTML = saida;
+    });
+}
+
